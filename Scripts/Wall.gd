0f8532b4eb8835c7ada32ef20d5e91dc417b028e
Nahumus <itaynahum2003@gmail.com>
2021-04-01 13:38:20 +0300

    changed color to name to prevent mistakes

--- Scripts/Wall.gd
+++ Scripts/Wall.gd
@@ -8,13 +8,13 @@ func _ready():
 	pass
 
 # Called on the wall's initialization
-func init(pos : Vector2, color = 'x'):
+func init(pos : Vector2, name = 'x'):
 	var tile : Tile = load("res://Prefabs/Tile.tscn").instance()
 	tile.set_tile_board_pos(pos)
 	self.add_child(tile)
 	tile.apply_texture('res://Sprites/box.png', 0)
 	tiles.append(tile)
-	self.color = color
+	self.color = name
 
 # Triggers when clicking on the wall
 func on_click(tile : Tile):
