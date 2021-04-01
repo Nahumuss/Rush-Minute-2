63dd700bb9f34efd79543521d4c57260b7634775
Nahumus <itaynahum2003@gmail.com>
2021-04-01 13:37:52 +0300

    added undoredo feature

--- Scripts/MultiplayerGame.gd
+++ Scripts/MultiplayerGame.gd
@@ -7,6 +7,7 @@ var main_board : Board = null
 var enemy_board : Board = null
 var wait_thread : Thread = Thread.new()
 var update_thread : Thread = Thread.new()
+var undo_redo = UndoRedo.new()
 
 func _ready():
 	load_board()
@@ -26,11 +27,13 @@ func wait_for_start(args):
 			break
 
 	main_board = preload("res://Prefabs/Board.tscn").instance()
+	main_board.name = 'Board'
 	add_child(main_board)
 	main_board.scale = BOARD_SCALE
 	main_board.generate_from_string(level)
 
 	enemy_board = preload("res://Prefabs/Board.tscn").instance()
+	enemy_board.name = 'EnemeyBoard'
 	add_child(enemy_board)
 	enemy_board.scale = Vector2(1,1) - BOARD_SCALE
 	enemy_board.position = Vector2(BOARD_SCALE.x * 64 * 6,0)
@@ -47,7 +50,7 @@ func update_enemy_board(args):
 			elif new_board == 'lmfaololyoulostthatonerealhardgonext':
 				enemy_board.win()
 				print('hard')
-			else:
+			elif len(new_board) == 36:
 				print('New board = ' + new_board)
 				if enemy_board.update_board_from_string(new_board) == 'err':
 					print("ERROR")
@@ -68,19 +71,38 @@ func _input(event):
 		if event is InputEventKey and event.pressed and timer.time_left == 0:
 			timer.start()
 			if main_board.selected_car:
+				var can_move = false
+				var forward = false
 				if event.scancode == KEY_W or event.scancode == KEY_UP or event.scancode == KEY_D or event.scancode == KEY_RIGHT:
 					if main_board.selected_car.can_move(true):
-						main_board.selected_car.move(true)
-						update_main_board()
+						can_move = true
+						forward = true
 				if event.scancode == KEY_S or event.scancode == KEY_DOWN or event.scancode == KEY_A or event.scancode == KEY_LEFT:
 					if main_board.selected_car.can_move(false):
-						main_board.selected_car.move(false)
-						update_main_board()
+						can_move = true
+						forward = false
+				if can_move:
+					undo_redo.create_action('Move car')
+					undo_redo.add_undo_property(main_board, 'level', main_board.level)
+					undo_redo.add_undo_method(self, 'update_main_board')
+					main_board.selected_car.move(forward)
+					main_board.level = str(main_board)
+					undo_redo.add_do_property(main_board, 'level', main_board.level)
+					undo_redo.add_do_method(self, 'update_main_board')
+					undo_redo.commit_action()
+					send_main_board()
 			if event.scancode == KEY_R:
 				main_board.soft_reset()
-				update_main_board()
+				undo_redo.clear_history()
+				send_main_board()
+			if event.scancode == KEY_Z and Input.is_key_pressed(KEY_CONTROL):
+				undo_redo.undo()
+			elif event.scancode == KEY_Y and Input.is_key_pressed(KEY_CONTROL):
+				undo_redo.redo()
 
 func update_main_board():
-	var level = main_board.to_string()
-	main_board.level = level
-	socket.put_utf8_string(level)
+	main_board.update_board_from_string(main_board.level)
+	send_main_board()
+
+func send_main_board():
+	socket.put_utf8_string(main_board.level)
