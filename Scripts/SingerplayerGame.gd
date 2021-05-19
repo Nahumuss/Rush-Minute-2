4367509124914092574992b4ca3b2a088b0040e6
Nahumus <itaynahum2003@gmail.com>
2021-05-19 21:10:25 +0300

    added a moves counter and accuracy

--- Scripts/SingerplayerGame.gd
+++ Scripts/SingerplayerGame.gd
@@ -3,8 +3,19 @@ extends Node2D
 var BOARD_SCALE = Vector2(0.75, 0.75)
 var main_board : Board = null
 var undo_redo = UndoRedo.new()
+var config = null
+var done_min_moves = 0
+var done_moves = 0
+var moves = 0
+var stats : VBoxContainer = VBoxContainer.new()
+var moves_counter_label : RichTextLabel = RichTextLabel.new()
+var avarage_accuracy_label : RichTextLabel = RichTextLabel.new()
+
+var config_path = "user://stats.cfg"
+var key = OS.get_unique_id()
 
 func _ready():
+	load_data()
 	var back_button = preload('res://Prefabs/BackButton.tscn').instance()
 	add_child(back_button)
 	main_board = preload("res://Prefabs/Board.tscn").instance()
@@ -14,11 +25,40 @@ func _ready():
 	main_board.position = Vector2(4/3 * 6 * 64 - BOARD_SCALE.x * 6 * 64,0)
 	main_board.generate_tiles()
 	
+	moves_counter_label.rect_min_size = Vector2(0,20)
+	moves_counter_label.text = "Moves: " + str(moves)
+	avarage_accuracy_label.rect_min_size = Vector2(0,30)
+	avarage_accuracy_label.text = "Accuracy: " + ((str(stepify(done_min_moves / float(done_moves) * 100.0, 0.1)) + '%') if done_moves != 0 else 'NAN')
+	stats.set_position(Vector2(4,50))
+	stats.set_size(Vector2((1 - BOARD_SCALE.x) * 64 * 6 - 8, 64 * 4.5 - 50))
+	stats.add_child(moves_counter_label)
+	stats.add_child(avarage_accuracy_label)
+	for child in stats.get_children():
+		var text = child.text
+		child.set_use_bbcode(true)
+		child.set_bbcode("[font=res://Fonts/statsfont.tres]"+text+"[/font]")
+	add_child(stats)
+
+func load_data() -> void:
+	config = ConfigFile.new()
+	var err = config.load_encrypted_pass(config_path, key)
+	if err == OK:
+		done_min_moves = int(config.get_value("user", "min_moves", 0))
+		done_moves = int(config.get_value("user", "done_moves", 0))
+
 func on_click(var board):
 	return true
 	
 func end_game() -> void:
+	config.set_value("user", "min_moves", done_min_moves + main_board.min_moves)
+	config.set_value("user", "done_moves", done_moves + moves)
+	done_min_moves += main_board.min_moves
+	done_moves += moves
+	moves = 0
+	moves_counter_label.set_bbcode("[font=res://Fonts/statsfont.tres]"+"Moves: 0"+"[/font]")
+	avarage_accuracy_label.set_bbcode("[font=res://Fonts/statsfont.tres]"+"Accuracy: " + ((str(stepify(done_min_moves / float(done_moves) * 100.0, 0.1)) + '%') if done_moves != 0 else 'NAN')+"[/font]")
 	main_board.start_new_level()
+	config.save_encrypted_pass(config_path, key)
 
 # Triggered on a key input
 func _input(event):
@@ -45,18 +85,24 @@ func _input(event):
 				undo_redo.add_do_property(main_board, 'level', main_board.level)
 				undo_redo.add_do_method(self, 'update_main_board')
 				undo_redo.commit_action()
+				moves += 1
 		if event.scancode == KEY_R:
-			main_board.soft_reset()
+			main_board.hard_reset()
+			main_board.generate_from_string(main_board.level_start)
 			undo_redo.clear_history()
-		if event.scancode == KEY_Z and Input.is_key_pressed(KEY_CONTROL):
+			moves += 1
+		if event.scancode == KEY_Z and Input.is_key_pressed(KEY_CONTROL) and undo_redo.has_undo():
 			undo_redo.undo()
-		elif event.scancode == KEY_Y and Input.is_key_pressed(KEY_CONTROL):
+			moves += 1
+		elif event.scancode == KEY_Y and Input.is_key_pressed(KEY_CONTROL) and undo_redo.has_redo():
 			undo_redo.redo()
+			moves += 1
+		moves_counter_label.set_bbcode("[font=res://Fonts/statsfont.tres]"+ "Moves: " + str(moves)+"[/font]")
 	if str(main_board)[17] == 'A':
 		main_board.win()
 		
 func close():
-	pass
+	config.save_encrypted_pass(config_path, key)
 		
 func update_main_board():
 	main_board.update_board_from_string(main_board.level)
